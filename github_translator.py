26bca1b06851d5adda6dce0b7270d3f35eff3ab7
egorandreevich <45241457+egorandreevich@users.noreply.github.com>
2018-12-06 16:55:39 +0000

    Add files via upload

---
+++ github_translator.py
@@ -0,0 +1,43 @@
+import requests
+import os
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# for file in (./):
+#     print(file)
+
+
+def translate_it(text_to_translate, language):
+    """
+    YANDEX translation plugin
+    docs: https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
+    https://translate.yandex.net/api/v1.5/tr.json/translate ?
+    key=<API-ключ>
+     & text=<переводимый текст>
+     & lang=<направление перевода>
+     & [format=<формат текста>]
+     & [options=<опции перевода>]
+     & [callback=<имя callback-функции>]
+    :param text: <str> text for translation.
+    :return: <str> translated text.
+    """
+    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
+
+    params = {
+        'key': key,
+        'lang': language,
+        'text': text_to_translate,
+    }
+    response = requests.get(url, params=params).json()
+    return ' '.join(response.get('text', []))
+
+def translate_and_save():
+    open_text = input('Введите путь текста:\n')
+    path = input('Введите путь для сохранения перевода:\n')
+    lang = input('Введите языки для перевода (пример: de-ru):\n')
+    with open (open_text) as text:
+        f = open(path, 'w')
+        f.write(translate_it(text, lang))
+        f.close()
+
+if __name__ == '__main__':
+    translate_and_save()
